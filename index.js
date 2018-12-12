0f28a6cb34b446d5113fd1ca1c790c80a0ba325b
Amanda Waller <amanda.waller.33@gmail.com>
2018-12-12 17:35:27 -0500

    added function to push recipe data on DOM to display

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ let inputWhatForm = "test what dessert to get";
 let inputLocation = "19125";
 const keyRecipe = "0e24783ea0e0e61d97038231191d5131";
 const idRecipe = "190b4f83";
-const keyLocation = "AIzaSyCNNIw6onSvZl9Z-3KqPMItMDezJoEUw8Q"; 
+const keyLocationSecret = "24DY22E4YCK3GXZRWU2MU5SFWT2DUHPVPRR4FWXYDVAGT3ZR"; 
+const secretLocation = "ZBQVXBNWPLEWN55ZHJO0H0REJE35SIA1FFCLT5H2CSHZAJ3Z";
+
+
+
+
 
 // HIDE/DISPLAY SECTIONS
 // hide start, show how WORKS
@@ -15,6 +20,10 @@ $("#formstart").submit(event => {
     $("#sectionstart").addClass("hidden");
     $("#sectionhow").removeClass("hidden");
 })
+
+
+
+
 // hide how, show what WORKS
 $("#formhow").click(event => {
     event.preventDefault();
@@ -23,42 +32,98 @@ $("#formhow").click(event => {
     $("#sectionhow").addClass("hidden");
     $("#sectionwhat").removeClass("hidden");
 })
+
+
+
+
+
 // hide what, show recipes or locations
 $("#formwhat").click(event => {
     event.preventDefault();
     inputWhatForm = $("#formwhat input[type=radio]:checked").val();
     console.log(inputWhatForm);
-    if (inputHowForm === "buy") {
-        getLocations();
+    if (inputHowForm === "make") {
+        getRecipes();
+        $("#sectionwhat").addClass("hidden");
+        $("#sectionrecipes").removeClass("hidden");
     }
     else {
-        getRecipes();
-    }
-    $("#sectionhow").addClass("hidden");
-    $("#sectionwhat").removeClass("hidden");
+        getZipcode();
+        console.log(inputLocation);
+        getLocations();
+        displayLocations(responseJson);
+        $("#sectionhow").addClass("hidden");
+        $("#sectionlocations").removeClass("hidden");
+    };
 })
 
 
-function getZipcode() {
-// get user zipcode and create variable from it
+
+
+
+
+
+
+
+// MODAL WINDOW
+// open modal window and get zipcode
+function openModal() {
+
 }
 
+
+function getZipcode() {
+    $("#formzipcode").click(event => {
+        event.preventDefault();
+        inputLocation = $("#formzipcode input[type=text]").text();
+    })
+    console.log(inputLocation);
+}
+
+
+
+
+
+
+
+// APIS
 function getRecipes() {
     fetch("https://api.edamam.com/search?app_id=" + idRecipe + "&app_key=" + keyRecipe + "&q=" + inputWhatForm + "&from=0&to=5")
     .then(response => response.json())
-    .then(responseJson => console.log(responseJson)
-    .catch(error => alert('Something went wrong. Try again later.'))
+    .then(responseJson => displayRecipes(responseJson)
+    //.then(displayRecipes(responseJson))
     );  
 }
 
 function getLocations() {
-    fetch("https://maps.googleapis.com/maps/api/place/textsearch/json?output=json&key=" + keyLocation + "&libraries=places&query=" + inputWhatForm + "+near+" + inputLocation + "&radius=32187")
+    fetch("https://api.foursquare.com/v2/venues/search?client_id=" + keyLocation + "&client_secret=" + keyLocationSecret + "&v=20201230&intent=browse&limit=20&query=" + inputWhatForm + "&near=" + inputLocation + "&radius=30000")
     .then(response => response.json())
     .then(responseJson => console.log(responseJson)
-    .catch(error => alert('Something went wrong. Try again later.'))
     );    
 }
 
+
+
+
+
+// WORKING WITH DATA
+function displayRecipes(responseJson) {
+    console.log("displayRecipes works");
+    // iterate through the items array and push them to DOM
+    for (let i = 0; i < responseJson.hits.length; i++) {
+        console.log("for loop works");
+        $("#recipesheader").after(
+            `<li><a href="${responseJson.hits[i].recipe.url}">${responseJson.hits[i].recipe.label}</a></h3>
+            <img src="${responseJson.hits[i].recipe.image}"></li>`
+        )};
+}
+
+function displayLocations() {
+    console.log("displayLocations working");
+}
+
+
+// ON PAGE LOAD
 $(function() {
     console.log("App loaded! Waiting for submit!");
     console.log(inputHowForm);
